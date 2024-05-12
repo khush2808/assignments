ef17a5c059000c39597a509f1c02af4426d71efd
Khush-Shah <khushshah675@gmail.com>
2024-05-12 16:31:55 +0530

    just testing string  and array functions

---
+++ 01-js/newfile.js
@@ -0,0 +1,10 @@
+/* using for understanding basic functions  and  object types in js
+*/
+// trim
+function trimString(s) {
+    console.log("Original String:", s);
+    console.log("After trim:", s.trim());
+    console.log("After trim:", s);
+  }
+  trimString("   Hello      World   ");
+  
